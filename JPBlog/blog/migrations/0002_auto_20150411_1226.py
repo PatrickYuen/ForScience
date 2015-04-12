971648f175b33adc6b20a73e02e03bdcd49f4f01
Patrick Yuen <ply209@nyu.edu>
2015-04-11 20:42:15 -0400

    bootstrap

---
+++ JPBlog/blog/migrations/0002_auto_20150411_1226.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='posts',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('author', models.CharField(max_length=200)),
+                ('title', models.CharField(max_length=200)),
+                ('body', models.TextField()),
+                ('slug', models.SlugField(max_length=200)),
+                ('publish', models.BooleanField(default=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+                'ordering': ['-created'],
+                'verbose_name': 'Blog Entry',
+                'verbose_name_plural': 'Blog Entries',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.DeleteModel(
+            name='post',
+        ),
+    ]
